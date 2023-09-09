7508c1e6806890fc40a51a11b5b2d5d227f099c5
Harsh <hshah3056@gmail.com>
2023-09-09 21:08:09 +0530

    image of banner and breadcrumb change

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -4,7 +4,7 @@
 
 @section('page-content')
 
-    <section class="page-title-section bg-img cover-background" data-overlay-dark="6" data-background="/website-assets/img/bg/bread_crumb_bg.jpg" style="background-image: url(/website-assets/img/bg/bg-03.jpg);">
+    <section class="page-title-section bg-img cover-background" data-overlay-dark="6" data-background="/website-assets/img/bg/bread_crumb_bg.jpg" style="background-image: url(/website-assets/img/bg/bread_crumb_bg.jpg);">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
